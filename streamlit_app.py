1c3182eca15c6cd068e1e3edd8e5d2389088f77f
Mohammad.Y <mohammad@atlasai.us>
2022-05-25 11:43:19 -0500

    feat: add authorization

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+import os
+from streamlit_auth0_service.auth0_service import StreamlitAuth0Service
 
 """
 # Welcome to Streamlit!
@@ -14,25 +16,32 @@ forums](https://discuss.streamlit.io).
 
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
+ORG_ID = os.environ['ORG_ID']
 
+auth_service = StreamlitAuth0Service()
+is_authorized = auth_service.is_authorized(ORG_ID)
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+if not is_authorized:
+    st.write("Unauthorized!")
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+else:
+    with st.echo(code_location='below'):
+        total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
+        num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
-    points_per_turn = total_points / num_turns
+        Point = namedtuple('Point', 'x y')
+        data = []
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+        points_per_turn = total_points / num_turns
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+        for curr_point_num in range(total_points):
+            curr_turn, i = divmod(curr_point_num, points_per_turn)
+            angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
+            radius = curr_point_num / total_points
+            x = radius * math.cos(angle)
+            y = radius * math.sin(angle)
+            data.append(Point(x, y))
+
+        st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
+            .mark_circle(color='#0068c9', opacity=0.5)
+            .encode(x='x:Q', y='y:Q'))
