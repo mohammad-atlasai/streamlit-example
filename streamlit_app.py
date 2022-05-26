552b8fc5ab0bdde270508c1b1e86d4dc333beaa5
Mohammad.Y <mohammad@atlasai.us>
2022-05-26 00:49:49 -0500

    feat: update authorization usage to guard wrapper

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,25 +3,22 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
-from streamlit_auth0_service.auth0_service import StreamlitAuth0Service
+from streamlit_auth0_service.guard import authorized
 
-"""
-# Welcome to Streamlit!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-auth_service = StreamlitAuth0Service()
-is_authorized = auth_service.is_authorized()
-
-if not is_authorized:
-    st.write("Unauthorized!")
-
-else:
+@authorized
+def run():
+    st.write("")
+    """
+    # Welcome to Streamlit!
+    
+    Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+    
+    If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
+    forums](https://discuss.streamlit.io).
+    
+    In the meantime, below is an example of what you can do with just a few lines of code:
+    """
     with st.echo(code_location='below'):
         total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
         num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
@@ -40,5 +37,9 @@ else:
             data.append(Point(x, y))
 
         st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-            .mark_circle(color='#0068c9', opacity=0.5)
-            .encode(x='x:Q', y='y:Q'))
+                        .mark_circle(color='#0068c9', opacity=0.5)
+                        .encode(x='x:Q', y='y:Q'))
+
+
+if __name__ == '__main__':
+    run()
